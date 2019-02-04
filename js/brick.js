90a347f34fc4e28042201f0c00ee108f4e70019c
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-04 16:02:56 +0000

    exploring other options with making the block move - no luck so far

--- js/brick.js
+++ js/brick.js
@@ -1,10 +1,34 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(){
+// $(document).ready(function() {
+console.log("Hello");
+  // var midblockmove = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, themidbrick]
+  // function midblockline() {
+  //   for (var i = 0; i < midblockmove.length; i--) {
+  //     midblockmove[i]
+  //   }
+  // }
 
-  var 
+//An array for the brick to move on
+  var midblockrow = new Array();
+  var midblockmoving = new Object();
+  midblockrow[12] = [null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"];
+
+//Make a function for the brick to actually move
+[null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"]
+[null, null, null, null, null, null, null, null, null, null, "midblockmoving", null]
+[null, null, null, null, null, null, null, null, null, "midblockmoving", null, null]
+[null, null, null, null, null, null, null, null, "midblockmoving", null, null, null]
+[null, null, null, null, null, null, null, "midblockmoving", null, null, null, null]
+[null, null, null, null, null, null, "midblockmoving", null, null, null, null, null]
+[null, null, null, null, null, "midblockmoving", null, null, null, null, null, null]
+[null, null, null, null, "midblockmoving", null, null, null, null, null, null, null]
+[null, null, null, "midblockmoving", null, null, null, null, null, null, null, null]
+[null, null, "midblockmoving", null, null, null, null, null, null, null, null, null]
+[null, "midblockmoving", null, null, null, null, null, null, null, null, null, null]
+["midblockmoving", null, null, null, null, null, null, null, null, null, null, null]
 
 
 
-
-
+//Obstacle for brick to stop on, triggering Game over
 
 });
--- js/player.js
+++ js/player.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(){
+// $(document).ready(function() {
 
 
 
