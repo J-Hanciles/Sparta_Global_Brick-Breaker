dfbf505f27f98a92973e66257a083cd7d62b558d
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-04 20:57:44 +0000

    removing some empty rows on the html in place of line breaks instead, should yield more visible results

--- js/brick.js
+++ js/brick.js
@@ -9,25 +9,30 @@ console.log("Hello");
   // }
 
 //An array for the brick to move on
-  var midblockrow = new Array();
-  var midblockmoving = new Object();
-  midblockrow[12] = [null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"];
+  // var midblockrow = new Array();
+  // var midblockmoving = new Object();
+  // midblockrow[12] = [null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"];
 
 //Make a function for the brick to actually move
-[null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"]
-[null, null, null, null, null, null, null, null, null, null, "midblockmoving", null]
-[null, null, null, null, null, null, null, null, null, "midblockmoving", null, null]
-[null, null, null, null, null, null, null, null, "midblockmoving", null, null, null]
-[null, null, null, null, null, null, null, "midblockmoving", null, null, null, null]
-[null, null, null, null, null, null, "midblockmoving", null, null, null, null, null]
-[null, null, null, null, null, "midblockmoving", null, null, null, null, null, null]
-[null, null, null, null, "midblockmoving", null, null, null, null, null, null, null]
-[null, null, null, "midblockmoving", null, null, null, null, null, null, null, null]
-[null, null, "midblockmoving", null, null, null, null, null, null, null, null, null]
-[null, "midblockmoving", null, null, null, null, null, null, null, null, null, null]
-["midblockmoving", null, null, null, null, null, null, null, null, null, null, null]
 
+//attempt1? - I don't what is the correct way to create this function to have the "object moving - as I have yet to assign the png image to the object, which I think is what I'm meant to do first"
 
+// [null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"]
+// [null, null, null, null, null, null, null, null, null, null, "midblockmoving", null]
+// [null, null, null, null, null, null, null, null, null, "midblockmoving", null, null]
+// [null, null, null, null, null, null, null, null, "midblockmoving", null, null, null]
+// [null, null, null, null, null, null, null, "midblockmoving", null, null, null, null]
+// [null, null, null, null, null, null, "midblockmoving", null, null, null, null, null]
+// [null, null, null, null, null, "midblockmoving", null, null, null, null, null, null]
+// [null, null, null, null, "midblockmoving", null, null, null, null, null, null, null]
+// [null, null, null, "midblockmoving", null, null, null, null, null, null, null, null]
+// [null, null, "midblockmoving", null, null, null, null, null, null, null, null, null]
+// [null, "midblockmoving", null, null, null, null, null, null, null, null, null, null]
+// ["midblockmoving", null, null, null, null, null, null, null, null, null, null, null]
+
+//attempt2? -
+// var midblockrow = [null, null, null, null, null, null, null, null, null, null, null, "midblockmoving"];
+// var midblockmove = (myFunction);
 
 //Obstacle for brick to stop on, triggering Game over
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,58 @@
 document.addEventListener("DOMContentLoaded", function(){
 // $(document).ready(function() {
+  function theBrick(){
+    document.getElementById("midblock12").style.backgroundColor = "firebrick"
 
+    var brick = [midblock1, midblock2, midblock3, midblock4, midblock5, midblock6, midblock7, midblock8, midblock9, midblock10, midblock11, midblock12];
+    var i = 12;
+    setInterval( function meh(){
+      console.log(brick[i-- % brick.length]);
+
+    }, 1000);
+
+
+      document.getElementById(meh()).style.backgroundColor = "firebrick";
+    //
+    // for(var i = 12; i >= 1; i-- ){
+    //   (function(i) {
+    //     setInterval(function(){
+    //       var id = i;
+    //       console.log(id)
+    //     }, 5000)
+    //   })(i);
+    //   }
+      // var id = "midblock" + i;
+      // console.log(id);
+      // document.getElementById(id).style.backgroundColor = "firebrick";
+      // document.getElementById(id+1).style.backgroundColor = "white";
+
+    }
+
+  //   var list = [1, 2, 3, 4, 5];
+  //
+  //   for (var i = 0, len = list.length; i < len; i += 1) {
+  //       (function(i) {
+  //           setInterval(function() {
+  //               list[i] += 10;
+  //               console.log(i + "=>" + list[i] + "\n");
+  //           }, 5000)
+  //       })(i);
+  //   }
+  //
+  //   var setInterval(function {
+  //
+  //     var setTimeout(function () {
+  //       for(var i = 12; i >= 1; i-- ){
+  //         var id = "midblock" + i;
+  //         console.log(id);
+  //         document.getElementById(id).style.backgroundColor = "firebrick";
+  //
+  //       }
+  //     }, 1000);
+  //
+  //   }, 1000);
+  // }
+
+  theBrick()
 
 });
