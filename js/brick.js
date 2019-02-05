61c0a9bf69fe6b4e88b9592114a6b34a8fe4d956
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-05 12:18:37 +0000

    Moved code to appropriate js file

--- js/brick.js
+++ js/brick.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
 // $(document).ready(function() {
 console.log("Hello");
+function theMidBrick(){
 
+
+  var brick = ["midblock1", "midblock2", "midblock3", "midblock4", "midblock5", "midblock6", "midblock7", "midblock8", "midblock9", "midblock10", "midblock11", "midblock12"];
+  var i = 0;
+  var brickmove = 0;
+  var bricktrail = 0;
+  var j = 0;
+
+  brickmove = setInterval(function meh(){
+    document.getElementById(brick[i]).style.backgroundColor = "firebrick";
+    document.getElementById(brick[j]).style.backgroundColor = "darkgrey";
+
+    // console.log(brick[i]);
+    // console.log(brick[j]);
+    i=i+1;
+    j=i-1;
+    if(i==10) {
+      i=0
+    }
+  }, 500);
+}
+
+theMidBrick()
 });
