e38e1e5865e22e500a3c8f5fdb46ee11bf82e23f
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-02-06 15:06:23 +0000

    back to where I was with my initial game breaking problem, will continue to try and figure out if else statment as opposed to attempt at swtich statement

--- js/brick.js
+++ js/brick.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 // $(document).ready(function() {
 console.log("Hello");
-  var midbrick = ["midblock1", "midblock2", "midblock3", "midblock4", "midblock5", "midblock6", "midblock7", "midblock8", "midblock9", "midblock10", "midblock11", "midblock12"];
-
-  var highbrick = ["highblock1", "highblock2", "highblock3", "highblock4", "highblock5", "highblock6", "highblock7", "highblock8", "highblock9", "highblock10", "highblock11", "highblock12"];
-
-  var lowbrick = ["lowblock1", "lowblock2", "lowblock3", "lowblock4", "lowblock5", "lowblock6", "lowblock7", "lowblock8", "lowblock9", "lowblock10", "lowblock11", "lowblock12"];
+  var brick = ["midblock1", "midblock2", "midblock3", "midblock4", "midblock5", "midblock6", "midblock7", "midblock8", "midblock9", "midblock10", "midblock11", "midblock12"];
 
 function theMidBrick(){
   var i = 0;
@@ -14,8 +10,8 @@ function theMidBrick(){
   var brickblock = 0;
 
   brickmove = setInterval(function meh(){
-    document.getElementById(midbrick[i]).style.backgroundColor = "firebrick";
-    document.getElementById(midbrick[j]).style.backgroundColor = "darkgrey";
+    document.getElementById(brick[i]).style.backgroundColor = "firebrick";
+    document.getElementById(brick[j]).style.backgroundColor = "darkgrey";
 
     // console.log(brick[i]);
     // console.log(brick[j]);
@@ -36,30 +32,6 @@ document.onkeydown = function playermidpunch(event){
     document.getElementById("midblock10").style.backgroundColor = "royalblue";
   }
 }
-
-// function theHighBrick(){
-//   var i = 0;
-//   var brickmove = 0;
-//   var j = 0;
-//   var brickblock = 0;
-//
-//   brickmove = setInterval(function meh(){
-//     document.getElementById(highbrick[i]).style.backgroundColor = "firebrick";
-//     document.getElementById(highbrick[j]).style.backgroundColor = "darkgrey";
-//
-//     // console.log(brick[i]);
-//     // console.log(brick[j]);
-//     i=i+1;
-//     j=i-1;
-//
-//     if(i==10) {
-//         console.log("Game Over");
-//       i=0
-//     }
-//   }, 250);
-// }
-
-
 
 
 
